9c29933f1d82baef26af2e8c1f09e257725d3ede
Douglas Prado <douglasrafaeldoprado@gmail.com>
2024-09-16 16:07:48 -0300

    task: cancelamento de checkin pre cadastro e pedido

--- app/app/(dashboard)/site/[id]/_components/sites.tsx
+++ app/app/(dashboard)/site/[id]/_components/sites.tsx
@@ -30,7 +30,7 @@ export default async function Sites({ limit }: { limit?: number }) {
 
   return sites.length > 0 ? (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-4">
-      {sites.map((site) => (
+      {sites.map((site: any) => (
         <SiteCard key={site.id} data={site} />
       ))}
     </div>
